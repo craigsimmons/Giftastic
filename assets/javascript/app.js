70dcc6bea67870f873ba4326174e20b5100ee155
Craig Simmons <craig.a.simmons@gmail.com>
2019-04-07 19:49:58 -0700

    Fixed lots of logic. 75% done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,51 +1,98 @@
 $(document).ready(function() {
 
     /* Global variable declarations */
-    var urlStr = "https://api.giphy.com/v1/gifs/search?"
-    var apiKeyStr = "dkQ0WtKAajp9NhC80EekhFeAswJO7B9j";
     var limitStr = 10;
     var ratingStr = "G";
     var topicsArr = ['dog', 'cat', 'fuck', 'bat', 'wolf'];
     var topicStr = "";
-    var queryURL = "";
 
-    /* Event Handlers */
-    $("#submit-form").click(function() {
+    /* Event Handlers and functions to run immediately */
+    makeButtons();
+
+    $("#submit-form").on("submit", function(event) {
         event.preventDefault();
-        buildAPIQuery();
-        makeButtons();
+        var answer = $("#submit-text").val();
+        if (answer === "") {
+            return false;
+        }
+        topicsArr.push(answer);
+        console.log(answer);
+        newButton(answer);
+        console.log(topicsArr);
     });
 
+    $(document).on("click", ".submitable", function(event) {
+        event.preventDefault();
+        var answer = $(this).attr("data");
+        console.log(answer);
+        buildAPIQuery(answer);
+    });
 
-    function buildAPIQuery() {
-        //topicStr = $(this).attr("#input-topic");
-        topicStr = "snake"
-        queryStr = (urlStr + "q=" + topicStr + "&api_key=" + apiKeyStr + "&rating=" + ratingStr + "&limit=" + limitStr);
-        console.log(topicStr);
-        console.log(queryStr);
-        makeAPIRequest();
-    }
-
-    function makeAPIRequest() {
-        $.ajax({
-                url: queryStr,
-                method: "GET",
-            })
-            .then(function(result) {
-                gifyObj = result;
-                console.log(gifyObj);
-            });
-    }
+/* functions   */
 
     function makeButtons() {
         $("#buttons-here").empty();
         for (var i = 0; i < topicsArr.length; i++) {
-            giphyButton = $("<button>");
-            console.log(giphyButton);
-            giphyButton.addClass("custombtn btn btn-primary");
+            var giphyButton = $("<button>");
+            giphyButton.addClass("submitable custombtn btn btn-primary");
             giphyButton.attr("data", topicsArr[i]);
             giphyButton.text(topicsArr[i]);
             $("#buttons-here").append(giphyButton);
         }
     }
-});
+
+    function newButton(topicStr) {
+        var giphyButton = $("<button>");
+        giphyButton.addClass("custombtn btn btn-primary submitable");
+        giphyButton.attr("data", topicStr);
+        giphyButton.text(topicStr);
+        $("#buttons-here").append(giphyButton);
+    }
+
+    function buildAPIQuery(topicStr) {
+        const urlStr = "https://api.giphy.com/v1/gifs/search?";
+        const apiKeyStr = "dkQ0WtKAajp9NhC80EekhFeAswJO7B9j";    
+        console.log(topicStr);
+        let queryURL = (urlStr + "q=" + topicStr + "&api_key=" + apiKeyStr + "&rating=" + ratingStr + "&limit=" + limitStr);
+        console.log(queryURL);
+        makeAPIRequest(queryURL);
+    }
+
+    function makeAPIRequest(queryURL) {
+        $.ajax({
+                url: queryURL,
+                method: "GET",
+            })
+            .then(function(response) {
+                var result = response.data;
+                console.log(result);
+                if (result === "") {
+                    console.log("nada");
+                    return false;  
+                }
+                else {
+                console.log("oh yeah, theres 10 objects");
+                generateGIFs(result);
+                }
+            });
+    }
+
+    function generateGIFs(result) {
+        $("#gifsView").empty();
+        for (var i = 0; i<result.length; i++){
+            var gifImage = $("<img>");
+            var gifDiv = $("<div>");
+            var gifRating = $("<p>").text("Rating " + result[i].rating);
+            gifDiv.addClass("imgdisplay");
+            gifRating.addClass("ratingdisplay");
+			gifImage.attr("src", result[i].images.fixed_height_small_still.url);
+			gifImage.attr("data-still", result[i].images.fixed_height_small_still.url);
+			gifImage.attr("data-animate", result[i].images.fixed_height_small.url);
+			gifImage.attr("data-state", "still");
+			gifImage.addClass("image");
+            gifDiv.append(gifImage);
+            gifDiv.append(gifRating);
+			$("#gifs-here").prepend(gifDiv);
+        }
+    }
+}); 
